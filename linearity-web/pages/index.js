c53e4c45f34d4d1c35a667aea8b6bd84b664c6cd
thatretrodev <thatretrodev@outlook.com>
2021-05-23 19:26:08 -0400

    Add a simple web interface

--- linearity-web/pages/index.js
+++ linearity-web/pages/index.js
@@ -1,8 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Alert } from 'react-bootstrap';
 
 export default function Home() {
   return (
-    <p>Coming soon!</p>
+			<Container>
+				<br></br>
+				<Alert variant="warning">
+					WARNING: The web frontend for Linearity is currently a work-in-progress and should not be expected to be stable. Because of that, Please do not use this app without knowing what you are doing.
+				</Alert>
+			</Container>
   )
 }
