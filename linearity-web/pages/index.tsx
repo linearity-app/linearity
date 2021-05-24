b55f644c465a1ca022ca3ef6f201a19fdede4eac
thatretrodev <thatretrodev@outlook.com>
2021-05-23 20:52:41 -0400

    Fix indentation in some code

--- linearity-web/pages/index.tsx
+++ linearity-web/pages/index.tsx
@@ -1,15 +1,16 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/dark_mode.css';
 import { Container, Alert } from 'react-bootstrap';
 
 export default function Home() {
-  return (
-			<Container>
-				<br></br>
-				<Alert variant="warning">
-					WARNING: The web frontend for Linearity is currently a work-in-progress and should not be expected to be stable. Because of that, Please do not use this app without knowing what you are doing.
-				</Alert>
-			</Container>
-  );
+	return (
+		<Container>
+			<br></br>
+			<Alert variant="warning">
+				WARNING: The web frontend for Linearity is currently a work-in-progress and should not be expected to be stable. Because of that, Please do not use this app without knowing what you are doing.
+			</Alert>
+		</Container>
+	);
 }
